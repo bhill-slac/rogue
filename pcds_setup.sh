b082d175ed9f74319d69241641178c5e6a5327f1
Bruce Hill <bhill@slac.stanford.edu>
2021-03-04 07:00:03 -0800

    Add pcds_setup.sh and README_BOOTSTRAP for pcds build support.

---
+++ pcds_setup.sh
@@ -0,0 +1,5 @@
+echo Setup build environment for rogue w/o python, conda or EPICS
+
+source $PSPKG_ROOT/etc/env_add_pkg.sh cmake/3.15.0
+source $PSPKG_ROOT/etc/env_add_pkg.sh zeromq/4.1.5
+echo See README_BOOTSTRAP for build instructions
